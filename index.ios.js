63684882ca951d6bd63bce1e0f30a728337bbe7b
Riza Fahmi <rizafahmi@gmail.com>
2016-10-25 10:37:52 +0700

    Initial commit. Hello world to the react native world

---
+++ index.ios.js
@@ -0,0 +1,51 @@
+'use strict'
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class PropertyFinder extends Component {
+  render() {
+    return (
+      <Text style={styles.text}>
+        Welcome to React Native!
+      </Text>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  text: {
+    color: 'black',
+    backgroundColor: 'white',
+    fontSize: 30,
+    margin: 80
+  }
+});
+
+AppRegistry.registerComponent('PropertyFinder', () => PropertyFinder);
